a6f5211cd737c1c255caece55f68358ec720c2f1
Denis Golubovskiy <bukashk0zzz@me.com>
2022-02-05 15:36:08 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,9 @@
+# Taboola tag for Google Tag Manager Server Side
+
+Taboola server to server conversion tracking tag.
+
+### Description TBD
+
+## Open Source
+
+Taboola Tag for GTM Server Side is developed and maintained by [Stape Team](https://stape.io/) under the Apache 2.0 license.
---
+++ template.js
@@ -0,0 +1,98 @@
+﻿const sendHttpRequest = require('sendHttpRequest');
+const setCookie = require('setCookie');
+const parseUrl = require('parseUrl');
+const JSON = require('JSON');
+const getRequestHeader = require('getRequestHeader');
+const encodeUriComponent = require('encodeUriComponent');
+const getCookieValues = require('getCookieValues');
+const getEventData = require('getEventData');
+
+const logToConsole = require('logToConsole');
+const getContainerVersion = require('getContainerVersion');
+const containerVersion = getContainerVersion();
+const isDebug = containerVersion.debugMode;
+const isLoggingEnabled = determinateIsLoggingEnabled();
+const traceId = getRequestHeader('trace-id');
+
+if (data.type === 'page_view') {
+    const url = getEventData('page_location') || getRequestHeader('referer');
+
+    if (url) {
+        const value = parseUrl(url).searchParams[data.clickIdParameterName];
+
+        if (value) {
+            const options = {
+                domain: 'auto',
+                path: '/',
+                secure: true,
+                httpOnly: false
+            };
+
+            if (data.expiration > 0) options['max-age'] = data.expiration;
+
+            setCookie('taboola_cid', value, options, false);
+        }
+    }
+
+    data.gtmOnSuccess();
+} else {
+    const clickId = getCookieValues('taboola_cid')[0] || '';
+
+    if (clickId) {
+        let requestUrl = 'https://trc.taboola.com/actions-handler/log/3/s2s-action?click-id='+enc(clickId)+'&name='+enc(data.eventName)+'&revenue='+enc(data.revenue)+'&currency=' + enc(data.currencyCode);
+
+        if (isLoggingEnabled) {
+            logToConsole(JSON.stringify({
+                'Name': 'Taboola',
+                'Type': 'Request',
+                'TraceId': traceId,
+                'EventName': data.eventName,
+                'RequestMethod': 'GET',
+                'RequestUrl': requestUrl,
+            }));
+        }
+
+        sendHttpRequest(requestUrl, (statusCode, headers, body) => {
+            if (isLoggingEnabled) {
+                logToConsole(JSON.stringify({
+                    'Name': 'Taboola',
+                    'Type': 'Response',
+                    'TraceId': traceId,
+                    'EventName': data.eventName,
+                    'ResponseStatusCode': statusCode,
+                    'ResponseHeaders': headers,
+                    'ResponseBody': body,
+                }));
+            }
+
+            if (statusCode >= 200 && statusCode < 300) {
+                data.gtmOnSuccess();
+            } else {
+                data.gtmOnFailure();
+            }
+        }, {method: 'GET'});
+    } else {
+        data.gtmOnSuccess();
+    }
+}
+
+function enc(data) {
+    data = data || '';
+    return encodeUriComponent(data);
+}
+
+function determinateIsLoggingEnabled() {
+    if (!data.logType) {
+        return isDebug;
+    }
+
+    if (data.logType === 'no') {
+        return false;
+    }
+
+    if (data.logType === 'debug') {
+        return isDebug;
+    }
+
+    return data.logType === 'always';
+}
